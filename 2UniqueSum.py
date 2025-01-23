5e04ed4524e2acec026c5f46cb0e31148704f8f6
jaysonndungu <jaysongachiani@gmail.com>
2025-01-23 09:31:26 -0500

    Updated code for O(n) time complexity

--- 2UniqueSum.py
+++ 2UniqueSum.py
@@ -3,7 +3,7 @@
 
 
 myList=[1,10,8,3,2,5,7,2,-2,-1]
-
+'''
 def uniqueSum(myList):
     uniqueList=[]
     sum=0
@@ -15,7 +15,18 @@ def uniqueSum(myList):
 
 print(uniqueSum(myList))
 #Time Taken: 4 minutes
+'''
 
+def uniqueSum(myList):
+    uniqueDict={}
+    sum=0 #initializing sum to 0
+    for i in myList:
+        if i not in uniqueDict: #if the number has not been seen
+            uniqueDict[i]=1 #Adds a count to verify the number has been seen
+            sum+=i #adds the unique number to the initialized sum
+    return sum
+
+print(uniqueSum(myList))
 
 
 
--- 3FirstOccurence.py
+++ 3FirstOccurence.py
@@ -1,7 +1,7 @@
 #Time Complexity = O(n)
 #Space Complexity= O(n)
 
-
+'''
 def FirstOccurence(myString):
     uniqueList=[]
     stringOutput=""
@@ -12,7 +12,23 @@ def FirstOccurence(myString):
     return stringOutput
 
 print(FirstOccurence("Uber Career Prep"))
-
+'''
 #Time Taken= 5 minutes
+
+def FirstOccurence(myString):
+    myDict={}
+    #Using a list(to hold the return value) instead of a string for better memory allocation. Since a string is immutable, concantenation creates a copy and therefore more allocated space.
+    stringOutput=[]
+ 
+    for i in myString:  #iterate directly over the characters
+        if i not in myDict: #O(1) lookup
+            stringOutput.append(i)
+            myDict[i]=1 #sets a key,value pair
+
+#Making the list into a string for better memory allocation
+    return "".join(stringOutput)
+
+print(FirstOccurence("Uber Career Prep"))
+
 
 
